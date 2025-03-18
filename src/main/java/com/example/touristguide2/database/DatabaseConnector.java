a35384a2c65043259818453ef814e3e805220fb5
NajibJivo <nana0001@stud.kea.dk>
2025-03-18 23:30:28 +0100

    database for TouristGuide3 added

---
+++ src/main/java/com/example/touristguide2/database/DatabaseConnector.java
@@ -0,0 +1,24 @@
+package com.example.touristguide2.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnector {
+    private static final String URL = "jdbc:mysql://localhost:3306/min_touristguide_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = ""; /* Password for the database, (Insert MySQL password here) */
+
+
+    static{
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver"); // Read JDBC driver
+            } catch (ClassNotFoundException e) {
+               throw new RuntimeException("Fejl: JDBC driver kunne ikke indlæses", e);
+            }
+    }
+
+    public static Connection getConnection() throws SQLException {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
